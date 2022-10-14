e6af8acaffe8c44425201e2c533d8f6fa1ee6fe4
Thomas Gubeno <t.gubeno@fairvision.fr>
2022-10-14 11:52:43 +0200

    grille fonctionnelle

--- UI.gd
+++ UI.gd
@@ -1,6 +1,7 @@
 extends Control
 
 signal roll(unfrozenDices)
+signal choix(coup)
 @onready var dicesList = [get_node("CanvasLayer/DiceHand/Dice1"), get_node("CanvasLayer/DiceHand/Dice2"), get_node("CanvasLayer/DiceHand/Dice3"), get_node("CanvasLayer/DiceHand/Dice4"), get_node("CanvasLayer/DiceHand/Dice5")]
 var unfrozenDices = []
 var frozenDices = []
@@ -40,9 +41,17 @@ func update_dice_pos():
 		node.position.x = 330 + 100*unfrozen_id
 		unfrozen_id += 1
 
+func nouveau_coup():
+	print(frozenDices)
+	for node in frozenDices:
+		unfrozenDices.append(node)
+		node.modulate = Color(1, 1, 1)
+	frozenDices.clear()
+	update_dice_pos()
+	emit_signal("roll", unfrozenDices)
+	
 func _on_roll_pressed():
 	emit_signal("roll", unfrozenDices)
-
 
 func _on_main_update_hand(hand):
 	for i in range(5):
@@ -50,11 +59,90 @@ func _on_main_update_hand(hand):
 	get_node("CanvasLayer/ValeurMain").text = "Main réelle : " + str(hand)
 		
 
-
 func _on_yams_manager_update_combi(combinaisons):
-	get_node("CanvasLayer/Combinaisons").text = "Total des 1 : {0}\nTotal des 2 : {1}\nTotal des 3 : {2}
+	get_node("CanvasLayer/Coups/Total1").text = "Total des 1\n" + str(combinaisons["total1"]) + " points"
+	get_node("CanvasLayer/Coups/Total2").text = "Total des 2\n" + str(combinaisons["total2"]) + " points"
+	get_node("CanvasLayer/Coups/Total3").text = "Total des 3\n" + str(combinaisons["total3"]) + " points"
+	get_node("CanvasLayer/Coups/Total4").text = "Total des 4\n" + str(combinaisons["total4"]) + " points"
+	get_node("CanvasLayer/Coups/Total5").text = "Total des 5\n" + str(combinaisons["total5"]) + " points"
+	get_node("CanvasLayer/Coups/Total6").text = "Total des 6\n" + str(combinaisons["total6"]) + " points"
+	get_node("CanvasLayer/Coups/Brelan").text = "Brelan\n" + str(combinaisons["brelan"]) + " points"
+	get_node("CanvasLayer/Coups/Carre").text = "Carré\n" + str(combinaisons["carre"]) + " points"
+	get_node("CanvasLayer/Coups/Full").text = "Full\n" + str(combinaisons["full"]) + " points"
+	get_node("CanvasLayer/Coups/PSuite").text = "Petite suite\n" + str(combinaisons["p_suite"]) + " points"
+	get_node("CanvasLayer/Coups/GSuite").text = "Grande suite\n" + str(combinaisons["g_suite"]) + " points"
+	get_node("CanvasLayer/Coups/Chance").text = "Chance\n" + str(combinaisons["chance"]) + " points"
+	get_node("CanvasLayer/Coups/Yams").text = "Yams\n" + str(combinaisons["yams"]) + " points"
+	
+func _on_yams_manager_update_grille(grille):
+	get_node("CanvasLayer/Grille").text = "Grille \n\nTotal des 1 : {0}\nTotal des 2 : {1}\nTotal des 3 : {2}
 	Total des 4 : {3}\nTotal des 5 : {4}\nTotal des 6 : {5}\nBrelan : {6}\nCarré : {7}\nFull : {8}
 	Petite suite : {9}\nGrande suite : {10}\nChance : {11}\nYam's : {12}".format([
-	str(combinaisons[0]), str(combinaisons[1]), str(combinaisons[2]), str(combinaisons[3]), str(combinaisons[4]),
-	str(combinaisons[5]), str(combinaisons[6]), str(combinaisons[7]), str(combinaisons[8]), str(combinaisons[9]),
-	str(combinaisons[10]), str(combinaisons[11]), str(combinaisons[12])])
+	grille["total1"], grille["total2"], grille["total3"], grille["total4"], grille["total5"],
+	grille["total6"], grille["brelan"], grille["carre"], grille["full"], grille["p_suite"],
+	grille["g_suite"], grille["chance"], grille["yams"]])
+
+func _on_total_1_pressed():
+	emit_signal("choix", "total1")
+	nouveau_coup()
+	get_node("CanvasLayer/Coups/Total1").visible = false
+
+func _on_total_2_pressed():
+	emit_signal("choix", "total2")
+	nouveau_coup()
+	get_node("CanvasLayer/Coups/Total2").visible = false
+
+func _on_total_3_pressed():
+	emit_signal("choix", "total3")
+	nouveau_coup()
+	get_node("CanvasLayer/Coups/Total3").visible = false
+
+func _on_total_4_pressed():
+	emit_signal("choix", "total4")
+	nouveau_coup()
+	get_node("CanvasLayer/Coups/Total4").visible = false
+
+func _on_total_5_pressed():
+	emit_signal("choix", "total5")
+	nouveau_coup()
+	get_node("CanvasLayer/Coups/Total5").visible = false
+
+func _on_total_6_pressed():
+	emit_signal("choix", "total6")
+	nouveau_coup()
+	get_node("CanvasLayer/Coups/Total6").visible = false
+
+func _on_brelan_pressed():
+	emit_signal("choix", "brelan")
+	nouveau_coup()
+	get_node("CanvasLayer/Coups/Brelan").visible = false
+
+func _on_carre_pressed():
+	emit_signal("choix", "carre")
+	nouveau_coup()
+	get_node("CanvasLayer/Coups/Carre").visible = false
+
+func _on_full_pressed():
+	emit_signal("choix", "full")
+	nouveau_coup()
+	get_node("CanvasLayer/Coups/Full").visible = false
+
+func _on_p_suite_pressed():
+	emit_signal("choix", "p_suite")
+	nouveau_coup()
+	get_node("CanvasLayer/Coups/PSuite").visible = false
+
+func _on_g_suite_pressed():
+	emit_signal("choix", "g_suite")
+	nouveau_coup()
+	get_node("CanvasLayer/Coups/GSuite").visible = false
+
+func _on_chance_pressed():
+	emit_signal("choix", "chance")
+	nouveau_coup()
+	get_node("CanvasLayer/Coups/Chance").visible = false
+
+func _on_yams_pressed():
+	emit_signal("choix", "yams")
+	nouveau_coup()
+	get_node("CanvasLayer/Coups/Yams").visible = false
--- YamsManager.gd
+++ YamsManager.gd
@@ -16,10 +16,27 @@ var grille = {
 	"yams": -1
 }
 
+var combinaisons = {
+	"total1": -1,
+	"total2": -1,
+	"total3": -1,
+	"total4": -1,
+	"total5": -1,
+	"total6": -1,
+	"brelan": -1,
+	"carre": -1,
+	"full": -1,
+	"p_suite": -1,
+	"g_suite": -1,
+	"chance": -1,
+	"yams": -1
+}
+
 signal update_combi(combinaisons)
+signal update_grille(grille)
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	emit_signal("update_grille", grille)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -82,8 +99,22 @@ func hand_sum(hand: Array):
 
 
 func _on_main_update_hand(hand):
-	var combinaisons = [calcul_total_x(hand, 1),calcul_total_x(hand, 2), calcul_total_x(hand, 3), 
-	calcul_total_x(hand, 4), calcul_total_x(hand, 5), calcul_total_x(hand, 6),calcul_brelan(hand),
-	calcul_carre(hand), calcul_full(hand), calcul_p_suite(hand), calcul_g_suite(hand), 
-	calcul_chance(hand), calcul_yams(hand)]
+	combinaisons['total1'] = calcul_total_x(hand, 1)
+	combinaisons['total2'] = calcul_total_x(hand, 2)
+	combinaisons['total3'] = calcul_total_x(hand, 3)
+	combinaisons['total4'] = calcul_total_x(hand, 4)
+	combinaisons['total5'] = calcul_total_x(hand, 5)
+	combinaisons['total6'] = calcul_total_x(hand, 6)
+	combinaisons['brelan'] = calcul_brelan(hand)
+	combinaisons['carre'] = calcul_carre(hand)
+	combinaisons['full'] = calcul_full(hand)
+	combinaisons['p_suite'] = calcul_p_suite(hand)
+	combinaisons['g_suite'] = calcul_g_suite(hand)
+	combinaisons['chance'] = calcul_chance(hand)
+	combinaisons['yams'] = calcul_yams(hand)
 	emit_signal("update_combi", combinaisons)
+
+
+func _on_ui_choix(coup):
+	grille[coup] = combinaisons[coup]
+	emit_signal("update_grille", grille)
